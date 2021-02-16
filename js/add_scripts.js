9fe0d722bd7e440ff6ee6419e103984dd8a6ab72
Tomasz1026 <tomkacz71@gmail.com>
2021-02-16 12:23:52 +0100

    przeróbki kodu widoku serwisu

--- js/add_scripts.js
+++ js/add_scripts.js
@@ -21,82 +21,142 @@ $( document ).ready(function() {
     console.log(checkDesc(opisA));
 */
     
+    function numberWithCommas(value, separator) {
+        return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator)
+    }
+
+    function dateConversion(date)
+    {
+        let day = date.slice(0, $("#passive_date").html().search('-'))
+        date = date.slice(date.search('-')+1, date.length)
+        let month = date.slice(0, $("#passive_date").html().search('-'))
+        let year = date.slice(date.search('-')+1, date.length)
+
+        return year + "-" + month + "-" + day
+    }
+
+    function mileageConversion(mileage, howManyTimes)
+    { 
+
+        for(let i=0;i<howManyTimes;i++)
+        {
+            mileage = mileage.replace(".", '')
+            //console.log(mileage)
+            //console.log(mileage.search("."))
+        }
+        return parseInt(mileage)
+    }
 
     $("#edit").on('click', () => {
-        if($("#passive_textarea").css('display') == 'none') { 
-            $("#add").hide(); 
-            $("#save").hide(); 
+        
+        if($("textarea").css("display") == "block") {
+            $("#add").hide()
+            $("#save").hide()
             
-            $("#date_input").hide();
-            $("#mileage_input").hide();
+            $("#date_input").hide()
+            $("#mileage_input").hide()
 
-            $("#passive_date").show();
-            $("#passive_mileage").show();
+            $("#passive_date").show()
+            $("#passive_mileage").show()
 
-            $("#passive_textarea").show();
-            $("#richer_description textarea").hide();
-
-           
+            $("#passive_textarea").show()
+            $("textarea").hide()
         } else {
-            $("textarea").val($("#passive_textarea").html()); 
-            $("#passive_textarea").hide();
-            $("#richer_description textarea").show();
+            $("textarea").val($("#passive_textarea").html())
+            $("#passive_textarea").hide()
+            $("#richer_description textarea").show()
 
-            $("#date_input").show();
-            $("#mileage_input").show();
-
-            if(($("#passive_mileage").html()).search("tys.")!=-1)
-            {
-                $("#mileage_input").val(($("#passive_mileage").html()).replace(" tys.", "000"))
-            }
-
-            let fullDate = $("#passive_date").html();
-
-            let day = fullDate.slice(0, $("#passive_date").html().search('-'));
-            fullDate = fullDate.slice(fullDate.search('-')+1, fullDate.length);
-            let month = fullDate.slice(0, $("#passive_date").html().search('-'));
-            let year = fullDate.slice(fullDate.search('-')+1, fullDate.length);
+            $("#date_input").show()
+            $("#mileage_input").show()
             
-            $("#date_input").val(year+"-"+month+"-"+day);
+            $("#mileage_input").val(mileageConversion($("#passive_mileage").html(), 2))
 
-            $("#passive_date").hide();
-            $("#passive_mileage").hide();
+            $("#date_input").val(dateConversion($("#passive_date").html()))
 
-            $("#add").show(); 
-            $("#save").show(); 
+            $("#passive_date").hide()
+            $("#passive_mileage").hide()
+
+            $("#add").show()
+            $("#save").show()
         }
-
     })
 
     $("#save").on('click', () => {
+        
         $("#passive_textarea").html($("textarea").val());
 
-        //system konwertowania przebiegu
+        $("#passive_mileage").html(numberWithCommas(parseInt(mileageConversion($("#mileage_input").val(), 2)), "."))
 
-
-        let date = new Date( $("#date_input").val());
-
-        console.log()
+        let date = new Date($("#date_input").val())
 
         $("#passive_date").html(('0' + date.getDate()).slice(-2) +'-'+ ('0' + (date.getMonth()+1)).slice(-2) +'-'+ date.getFullYear());
 
-        $("#edit").click(); 
+        $("#edit").click()
     })
+
+    
 
     $(".category").mouseenter( function() {
 
-    $(this).find("div").show();
+        $(this).find(">:first-child").css("left", $(this).parent().innerWidth())
+        $(this).find(">:first-child").show()
+        
+        console.log($(this).outerWidth())
 
     }).mouseleave( function() {
     
-    $(this).find("div").hide();
+        $(this).find(">:first-child").hide()
 
     })
 
+    $(".category").on('click', function() {
+
+        if($(this).find("span").length == 0)
+        {
+            $("textarea").val($("textarea").val()+$(this).html()+"\n")
+        }
+    })
 
     $(".sub_list span").on('click', function() {
+        
+        let parentTree = $(this).parent()
+        
+        if(parentTree.attr("class") == "sub_list")
+        {
+        let categoriesHtml = [$(this).html()]
+        //let kaka = 0
+        //console.log(niba);
+        let arrayId = 1
+        do{
+            //console.log(kaka)
+            
+            
+            parentTree = parentTree.parent()
 
-    $("textarea").val($("textarea").val()+$(this).parents(".category").find("span:last").html()+" "+$(this).html()+"\n");
+            idOfParent = parentTree.attr('id')
+
+            if(parentTree.attr("class") == "category")
+            {
+                categoriesHtml[arrayId] = parentTree.find("span:last").html()
+                //console.log(niba);
+                //console.log(nigger);
+                    arrayId++
+            }
+            
+            //kaka++
+            
+            
+            
+
+        }while(parentTree.attr('id') != "list")
+            
+        for(let i=arrayId-1;i>=0;i--)
+        {
+            $("textarea").val($("textarea").val() + categoriesHtml[i] + " ")
+        }
+
+        $("textarea").val($("textarea").val()+"\n")
+    }
     })
 /*
     window.onbeforeunload = function() {
