dfe7381254950f18066740f344a6cc9d5b8bc2ef
Tomasz1026 <tomkacz71@gmail.com>
2021-03-20 15:01:48 +0100

    Ważne są tylko te dni, których jeszcze nie znamy

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 
-  $("#eye_svg").click()
-
-    $("#eye_svg").on('click', () => {
-      if ($("#pass_input input").attr("type") == "password") {
-          $("#eye_svg").attr("src","../img/crossed_out_eye.svg")
-          $("#pass_input input").attr("type", "text");
+    $(".eye_svg").on("click", function() {
+      if ($(this).parent().find("input").attr("type") == "password") {
+          $(this).attr("src","../img/crossed_out_eye.svg")
+          $(this).parent().find("input").attr("type", "text");
         } else {
-          $("#eye_svg").attr("src","../img/eye.svg")
-          $("#pass_input input").attr("type", "password");
+          $(this).attr("src","../img/eye.svg")
+          $(this).parent().find("input").attr("type", "password");
         }
     })
-
-
-
