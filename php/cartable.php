2a8b147f5b41218d0fba5cef8043550f14bf0b59
Tomasz1026 <tomkacz71@gmail.com>
2021-04-10 19:26:12 +0200

    That's the story of the Hurricane

--- php/cartable.php
+++ php/cartable.php
@@ -2,11 +2,11 @@
     require_once "data_base_connections/conn.php";
 
     session_start();
-    if(!isset($_SESSION['user_logged']))
+    if(empty($_SESSION['user_logged']))
     {
         header("Location: index.php");
         exit();
-    } else if(isset($_SESSION['car_id'])) {
+    } else if(!empty($_SESSION['car_id'])) {
         unset($_SESSION['car_id']);
     }
 ?>
@@ -56,79 +56,72 @@
 
                 $new_array = "";
 
-                if(isset($_GET['filter'])) {
-
-                    $filter_elements = explode(".", $_GET['filter']);
-
-                    foreach($filter_elements as $f => $f_value) {
-                        if(!array_key_exists($f_value, $filter_array)) {
-                            header("Location: cartable.php?page=1&sort_by=last&sort_type=DESC");
-                            exit();
-                        }
-                        //$new_array .= $f_value.".";
-                    }
-                    $new_array = $_GET['filter'].".";
-                }
-
-                if(!empty($_GET['search_text'])) {
+                if(isset($_GET['search_text']) && strlen($_GET['search_text'])>0) {
                     echo htmlentities($_GET['search_text'], ENT_QUOTES, "UTF-8");
 
-                    //global $search_query;
-                    $search_query = "AND (";
-
-                    //$search_query_array = array("mark" => "car.mark ", "model" => "car.model ", "gen" => "car.generation ", "vin" => "car.vin ", "reg" => "car.registration ", "year" => "car.year ", "last" => "date ");
-                    //$search_query_array = array("car.mark ", "car.model ", "car.generation ", "car.vin ", "car.registration ", "car.year ", "date ");
+                    $search_query = " AND (";
 
                     if(!empty($_GET['filter'])) {
                       
-                            foreach($filter_elements as $f => $f_value) {
-                                
-                                if(array_key_exists($f_value, $filter_array)) {
-                                    $search_query .= $filter_array[$f_value][1]."LIKE '%".$_GET['search_text']."%'";
-                                    if($f != count($filter_elements)-1) {
-                                        $search_query .= " OR ";
-                                    }
-                                }
+                        $filter_elements = explode(".", $_GET['filter']);
+
+                        //echo implode('.',$filter_elements);
+
+                        //echo implode('/',array_diff_assoc($filter_elements, $filter_array));
+
+                        foreach($filter_elements as $index => $index_value) {
+                            if(!array_key_exists($index_value, $filter_array)) {
+                                header("Location: cartable.php?page=1&sort_by=last&sort_type=DESC");
+                                exit();
                             }
+
+                            $search_query .= $filter_array[$index_value][1]."LIKE '%".$_GET['search_text']."%'";
                             
+                            if($index != count($filter_elements)-1) {
+                                $search_query .= " OR ";
+                            }
+
+                            $new_array .= $index_value.".";
+                        }
+                        
                     } else {
-                        foreach($filter_array as $i => $i_value) {
-                            $search_query .= $i_value[1]."LIKE '%".$_GET['search_text']."%'";
+                        foreach($filter_array as $index => $index_value) {
+                            $search_query .= $index_value[1]."LIKE '%".$_GET['search_text']."%'";
                             
-                            if($i != "last") {
+                            if($index != "last") {
                                 $search_query .= " OR ";
                             }
                         }
                     }
 
                     $search_query .= ")";
+                }else if (!empty($_GET['filter'])) {
+                    header("Location: cartable.php?page=1&sort_by=last&sort_type=DESC");
+                    exit();
                 }
                 
                 echo ">";
             ?>
-            <div id="search_filter">
-                Filtry
-                <span id='list'>
-                    <?php
-                        foreach($filter_array as $a => $a_value) {
-                            echo "<span name='$a'>$a_value[0]";
+            <div id="search_filter">Filtry</div>
+            <span class="filter" id='list'>
+                <?php
+                    foreach($filter_array as $index => $index_value) {
+                        echo "<span name=".$index.">".$index_value[0];
 
-                            if(array_search($a, $filter_elements) != "") {
-                                echo "<img src='../img/check.svg' style='margin-left: 10px;' height='10px'>";
-                            }
-                            echo "</span><hr></hr>";
+                        if(array_search($index, $filter_elements) != "") {
+                            echo "<img src='../img/check.svg' style='margin-left: 10px;' height='10px'>";
                         }
+                        echo "</span><hr></hr>";
+                    }
 
-                        echo "<span id='filter_accept'>Zastosuj filtry</span>";
-                    ?>
-                </span>
-            </div>
+                    echo "<span id='filter_accept'>Zastosuj filtry</span>";
+                ?>
+            </span>
             <div id="service_table">
                 <div id='table_header'>
-                    <?php    
-                        $sorting = "date DESC";
+                    <?php
 
-                        if(isset($_GET['sort_by']) && isset($_GET['sort_type'])) {
+                        if(!empty($_GET['sort_by']) && !empty($_GET['sort_type'])) {
                             if((!array_key_exists($_GET['sort_by'], $filter_array)) || ($_GET['sort_type'] != "DESC" && $_GET['sort_type'] != "ASC")) {
                                 header("Location: cartable.php?page=1&sort_by=last&sort_type=DESC");
                                 exit();
@@ -139,7 +132,7 @@
                         }
                         
                         foreach($filter_array as $x => $x_value) {
-                            echo "<div id='table_header_$x' name='$x'>$x_value[0]";
+                            echo "<div id='table_header_$x' name='$x'>".$x_value[0];
                                 
                             if($x == $_GET['sort_by']) {
                                 $sorting = $filter_array[$_GET['sort_by']][1];
@@ -158,25 +151,13 @@
                     $connection = @new mysqli($host, $db_user, $db_pass, $db_name);
 
                     if($connection->connect_errno!=0) {//connect error id: 0 - everything fine, >0 - something went wrong
-                    
                         //echo "ERROR: ".$connection->connect_errno." Description: ".$connection->connect_error;//Show error number in browser. Disable 'Desccription' later
                         $_SESSION['error_alert'] = "Błąd ID ".$connection->connect_errno." Opis: ".$connection->connect_error;
                     } else {
 
                         $user_id = $_SESSION['id'];
-                        //$search_query = htmlentities($search_query, ENT_QUOTES, "UTF-8");
-                        /* sprintf("SELECT COUNT(id) AS number_of_pages FROM car WHERE client_id=%s %s",
-                        mysqli_real_escape_string($connection, $user_id),
-                        mysqli_real_escape_string($connection, $search_query));*/
 
-                        //echo mysqli_real_escape_string($connection, $search_query);
-                        
-                        /*echo sprintf("SELECT COUNT(id) AS number_of_pages FROM car WHERE client_id=%s %s",
-                        mysqli_real_escape_string($connection, $user_id),
-                        mysqli_real_escape_string($connection, $search_query)
-                        );*/
-
-                        if($result = @$connection->query("SELECT COUNT(id) AS number_of_pages FROM car WHERE client_id= $user_id $search_query")) {
+                        if($result = @$connection->query("SELECT COUNT(id) AS number_of_pages FROM car WHERE client_id=".$user_id.$search_query)) {
                             //echo "d";
                             $num = $result->fetch_assoc();
                             $num = ceil($num['number_of_pages']/10);
@@ -188,16 +169,13 @@
                         if($num == 0) {
                             $num = 1;
                         }
-
                         //echo $search_query."<br><br>";
                         $sorting = htmlentities($sorting, ENT_QUOTES, "UTF-8");
                         //echo $_GET['filter'];
-
-                        
-                        if(isset($_GET['page'])){
+                        if(isset($_GET['page'])) {
                             $active_page = $_GET['page'];
-                            if($active_page < 1) {$active_page = 1;}
-                            else if($active_page > $num) {$active_page = $num;}
+                            if($active_page < 1) {header("Location: cartable.php?page=1&sort_by=last&sort_type=DESC");}
+                            else if($active_page > $num) {header('Location: cartable.php?page='.$num.'&sort_by=last&sort_type=DESC');}
                             $page_number = 10 * ($active_page-1);
                             //$page_number = htmlentities($page_number, ENT_QUOTES, "UTF-8");
 
@@ -208,7 +186,6 @@
 
                             $sql_query_cars = sprintf("SELECT car.*,(SELECT service.date FROM service WHERE car.id=service.car_id LIMIT 1) AS date FROM car WHERE car.client_id=%s %s ORDER BY %s LIMIT 10 OFFSET 0", $user_id, $search_query, $sorting);
                         }
-                        
                         //echo $sql_query_cars;
 
                         if($result = $connection->query($sql_query_cars)) //Send query to database. If everything goes fine return value true and save data to result variable
@@ -228,7 +205,7 @@
                             } else {
                                 echo "<div class='row' style='text-align: center;'>Nie ma jeszcze żadnych pojazdów w twoim warsztacie</div>";
                             }
-                        } 
+                        }
                 ?>
             </div>
             <div id="page_count">
@@ -240,9 +217,7 @@
                     echo "<img id='left'>";
                 }
                
-                echo "<input name='page' type='number' min=1 max=$num value=$active_page required> z <span>$num</span>";
-                
-                
+                echo '<input name="page" type="number" min=1 max='.$num.' value='.$active_page.' required> z <span>'.$num.'</span>';
                 
                 if($active_page < $num) {
                     echo "<img class='arrow' id='right' src='../img/arrow.svg'>";
@@ -251,16 +226,14 @@
                 }
                  
                 $connection->close();
+                
+                $new_array = substr($new_array, 0 , -1);
+
+                echo '<input type="checkbox" name="sort_by" value='.$_GET['sort_by'].' checked>';
+                echo '<input type="checkbox" name="sort_type" value='.$_GET['sort_type'].' checked>';
             
-            if(isset($_GET['sort_by']) && isset($_GET['sort_type'])) {
-                echo "<input type='checkbox' name='sort_by' value='$_GET[sort_by]' checked>";
-                echo "<input type='checkbox' name='sort_type' value='$_GET[sort_type]' checked>";
-            } else {
-                echo "<input type='checkbox' name='sort_by' value='last_service' checked>";
-                echo "<input type='checkbox' name='sort_type' value='DESC' checked>";
-            }
-                echo "<input type='checkbox' name='filter' value='$new_array' checked>";
-                echo "<input type='submit'>";
+                echo '<input type="checkbox" name="filter" value='.$new_array.' checked>';
+                echo '<input type="submit">';
             }
             ?>
             </div>
